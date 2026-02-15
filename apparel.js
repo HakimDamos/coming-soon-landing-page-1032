8af2722b935b859cbbf9c3d5b229b3eebef1afaf
HakimDamos <109410864+HakimDamos@users.noreply.github.com>
2026-02-15 16:23:06 +0800

    Update the coming soon landing page file

---
+++ apparel.js
@@ -0,0 +1,17 @@
+// Javascript Form Validation
+const form = document.getElementById('newsForm');
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Shows the error state for a given input field.
+function showError() { };
+
+// Clears the error state for a given input field.
+function cleanError() { };
+
+// Validates a single input field.
+function cleanError() { }
+
+// Handles the form submission event
+form.addEventListener('submit', (e) => {
+    e.preventDefault;
+});
